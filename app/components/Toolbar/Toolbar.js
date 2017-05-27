9f70365faea37d2f574a7ada3d06a254dd931587
aditya2337 <aditya.n@gai.co.in>
2017-05-27 14:58:10 +0530

    centered the title on header

--- app/components/Toolbar/Toolbar.js
+++ app/components/Toolbar/Toolbar.js
@@ -9,9 +9,10 @@ export default class Header extends Component {
         leftElement='menu'
         centerElement='Sarcasm Society'
         onLeftElementPress={this.props.openDrawer}
+        style={{ centerElementContainer: { flex: 1, alignItems: 'center' } }}
         searchable={{
           autoFocus: true,
-          placeholder: 'Search',
+          placeholder: 'Search'
         }}
       />
     );
