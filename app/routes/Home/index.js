4fd3195eec33c5270b2e1edf3819bfde71725890
aditya2337 <aditya.n@gai.co.in>
2017-05-27 14:25:16 +0530

    removing react-router-flux and implementing react-navigation

--- app/routes/Home/index.js
+++ app/routes/Home/index.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import HomeScreen from './Home';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class Home extends Component {
+  static navigationOptions = {
+    title: 'Sarcasm Society',
+    header: null
+  };
+  
+  componentDidMount () {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000);
+  }
+
   render () {
     return <HomeScreen />;
   }
--- index.js
+++ index.js
@@ -1,51 +1,12 @@
 'use strict';
 
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
-import { Router, Scene } from 'react-native-router-flux';
+import { StackNavigator } from 'react-navigation';
 
 import Home from './app/routes/Home';
-import Header from './app/components/Header.js';
 
-export default class App extends Component {
+const App = StackNavigator({
+  Home: { screen: Home }
+},
+{headerMode: 'screen'});
 
-  componentDidMount () {
-    setTimeout(() => {
-      SplashScreen.hide();
-    }, 3000);
-  }
-
-  render () {
-    return (
-      <Router>
-        <Scene key='root' hideNavBar={true}>
-          <Scene key='pageOne' component={Home} initial={true} />
-        </Scene>
-      </Router>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
+export default App;
